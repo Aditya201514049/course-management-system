2ebbf1fe0e98b340d904acb3d430b568f5537ed1
Aditya Singha <adityasingha084@gmail.com>
2026-05-18 23:12:48 +0600

    Implement user model and set up routing for authentication, users, and courses in the Express app

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express")
+const cookieParser = require("cookie-parser")
+
+const authRoutes = require("./routes/authRoutes")
+const userRoutes = require("./routes/userRoutes")
+const courseRoutes = require("./routes/courseRoutes")
+
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
 app.use("/api/courses", courseRoutes)
 
 module.exports = app
